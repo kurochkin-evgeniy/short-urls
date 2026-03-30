d0a8f96882dcadda4870128ec2a40d7541298263
Курочкин Евгений <EIgoKurochkin@sberbank.ru>
2026-03-30 20:50:03 +0300

    Add compression

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"compress/flate"
 	"net/http"
 	"short-urls/internal/config"
 	"short-urls/internal/handler"
@@ -10,6 +11,7 @@ import (
 	"short-urls/internal/middleware"
 
 	"github.com/go-chi/chi/v5"
+	chi_m "github.com/go-chi/chi/v5/middleware"
 )
 
 type ShortUrlApp struct {
@@ -32,7 +34,12 @@ func (a *ShortUrlApp) Start() error {
 
 	r := chi.NewRouter()
 
+	compressor := chi_m.NewCompressor(flate.DefaultCompression, "/*")
+	r.Use(compressor.Handler)
+
 	r.Use(middleware.LoggingMiddleware)
+	r.Use(middleware.DecompressRequestMiddleware)
+
 	r.Post("/", handler.HandleCreateShortUrRequest(a.shortUrlService))
 	r.Post("/api/shorten", handler.HandleCreateShortUrRequest(a.shortUrlService))
 	r.Get("/{id}", handler.HandleRedirectRequest(a.shortUrlService))
--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"compress/gzip"
 	"net/http"
 	"short-urls/internal/logging"
 	"time"
@@ -19,5 +20,27 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 
 		logging.Sugar.Infof("Request: %s %s duration=%v", r.Method, r.URL.Path, duration)
 		logging.Sugar.Infof("Responce: %v size=%v", ww.Status(), ww.BytesWritten())
+	})
+}
+
+func DecompressRequestMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Content-Encoding") == "gzip" {
+			// Wrap the request body with a gzip reader
+			gz, err := gzip.NewReader(r.Body)
+			if err != nil {
+				http.Error(w, "Bad request", http.StatusBadRequest)
+				return
+			}
+			defer gz.Close()
+
+			// Replace the original request body with the decompressed one
+			r.Body = gz
+			// Also, update the Content-Length and remove Content-Encoding headers
+			r.Header.Del("Content-Encoding")
+			r.Header.Del("Content-Length")
+		}
+		// Continue to the next handler
+		next.ServeHTTP(w, r)
 	})
 }
