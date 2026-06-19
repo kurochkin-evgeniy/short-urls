23dd86bdb29eaf9bbb0a65861a0d69a8f80bc254
Евгений Курочкин <eigokurochkin@sberads.tech>
2026-06-19 20:10:19 +0300

    глобальные переменные

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"short-urls/internal/app"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 func main() {
+	printBuildInfo()
+
 	if err := run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func printBuildInfo() {
+	fmt.Fprintf(os.Stdout, "Build version: %s\n", valueOrNA(buildVersion))
+	fmt.Fprintf(os.Stdout, "Build date: %s\n", valueOrNA(buildDate))
+	fmt.Fprintf(os.Stdout, "Build commit: %s\n", valueOrNA(buildCommit))
+}
+
+func valueOrNA(value string) string {
+	if value == "" {
+		return "N/A"
+	}
+	return value
 }
 
 func run() error {
